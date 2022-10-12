c59cc20a718deced5893bf9ba6aa2354f4a53bfa
Paul Katumba <paulkatumba@minute5.ug>
2022-10-12 11:16:24 +0000

    first commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main{
+    public static void main(String[] args){
+        int examScore = 90;
+        double floatNumber = 1.0;
+        double floatFigure =0.9;
+        String name = null;
+        double sum = floatNumber+floatFigure;
+        System.out.println(examScore);
+    }
+
+
+}
---
+++ Test.java
@@ -0,0 +1,5 @@
+public class Test{
+    public static void another(){
+        System.out.println("Another string");
+    }
+}
