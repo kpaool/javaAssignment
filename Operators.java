fedc469ed3ffe317082dfab6bd52ead9b8a24de6
Paul Katumba <paulkatumba@minute5.ug>
2022-10-14 09:45:52 +0000

    Working on operations/ conditionals

---
+++ Operators.java
@@ -0,0 +1,19 @@
+public class Operators{
+    public static void main(String[] args){
+        double valueA=20;
+        int valueB=20;
+        valueB= 2;
+        valueB+=33;
+        int examScore =70;
+
+        if(examScore>=70){
+            System.out.println("You passed with an A");
+        }else if(examScore>=60){
+            System.out.println("You passed with an B");
+        }else{
+            System.out.println("You failed");
+        }
+        
+        
+    }
+}
