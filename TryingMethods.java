ba5a06176791cd41aff7279311b8e321dc29065a
Paul Katumba <paulkatumba@minute5.ug>
2022-10-14 11:49:49 +0000

    stable main

--- TryingMethods.java
+++ TryingMethods.java
@@ -1,6 +1,10 @@
 public class TryingMethods {
  
     public static void main(String[] args) {
-        
+        /*
+         * This class is to explain the use of methods together with different operators and conditionals
+         */
+
     }
+    
 }
