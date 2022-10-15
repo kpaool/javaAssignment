fcc3a5f8d1facf9916f2ee53052eb5bd564d8a5e
Paul Katumba <paulkatumba@minute5.ug>
2022-10-15 19:31:53 +0000

    printFiveTimes implemented

---
+++ TryingOutJava.java
@@ -0,0 +1,13 @@
+public class TryingOutJava {
+    public static void main(String[] args) {
+        printFiveTimes();
+    }
+
+    public static void printFiveTimes() {
+        int counter=0;
+        while (counter<5) {
+            System.out.println("I'm a string that is to be printed out five times");
+            counter++;
+        }
+    }
+}
